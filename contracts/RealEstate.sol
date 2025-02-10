a293c98fff29e6cb64e467b6d42d17ce6f3d2a61
nehhhhha6 <n0370525@gmail.com>
2025-02-10 10:23:19 +0530

    update

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -9,4 +9,20 @@ contract GameItem is ERC721URIStorage {
 
     constructor() ERC721("Real Estate", "REAL") {}
 
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
 }
