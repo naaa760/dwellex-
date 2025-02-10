dff2afe78ae66a0d3735162913968a305da86c0f
nehhhhha6 <n0370525@gmail.com>
2025-02-10 10:10:10 +0530

    update

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,3 +1,8 @@
 pragma solidity ^0.8.20;
 
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract GameItem is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+}
