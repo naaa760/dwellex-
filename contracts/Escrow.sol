c3eaad4f7a48802d0dfcd981f2f2f2ae21553778
nehhhhha6 <n0370525@gmail.com>
2025-02-10 15:07:47 +0530

    update

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,6 +15,23 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "only buyer can call this method");
+        _;
+    }
+
+
+
+
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "only seller can call this method");
+        _;
+    }
+
+
+
     mapping(uint256 => bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
@@ -40,15 +57,29 @@ contract Escrow {
         address _buyer,
         uint256 _purchasePrice,
         uint256 _escrowAmount
-    
-    
-    
-    ) public {
+
+
+    ) public payable onlySeller {
+
+
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
         isListed[_nftID] = true;
         purchasePrice[_nftID] = _purchasePrice;
         escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
     }
+
+    // Put Under Contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID){
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
 
 
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -36,7 +36,9 @@ describe("Escrow", () => {
     await transaction.wait();
 
     // List Property
-    transaction = await escrow.connect(seller).list(1);
+    transaction = await escrow
+      .connect(seller)
+      .list(1, buyer.address, tokens(10), tokens(5));
     await transaction.wait();
   });
 
@@ -80,6 +82,22 @@ describe("Escrow", () => {
     it("Returns purchase price", async () => {
       const result = await escrow.purchasePrice(1);
       expect(result).to.be.equal(tokens(10));
+    });
+
+    it("Returns escrow amount", async () => {
+      const result = await escrow.escrowAmount(1);
+      expect(result).to.be.equal(tokens(5));
+    });
+  });
+
+  describe("Deposits", () => {
+    it("Updates contract balance", async () => {
+      const transaction = await escrow
+        .connect(buyer)
+        .depositEarnest(1, { value: tokens(5) });
+      await transaction.wait();
+      const result = await escrow.getBalance();
+      expect(result).to.be.equal(tokens(5));
     });
   });
 });
