c2faaab210cb2ab2ee57459f067254911141debb
nehhhhha6 <n0370525@gmail.com>
2025-02-10 11:01:38 +0530

    update

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,28 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GameItem is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract RealEstate is ERC721URIStorage {
+    uint256 private _tokenIds;
 
     constructor() ERC721("Real Estate", "REAL") {}
 
     function mint(string memory tokenURI) public returns (uint256) {
-        _tokenIds.increment();
-
-
-        uint256 newItemId = _tokenIds.current();
+        _tokenIds++;
+        uint256 newItemId = _tokenIds;
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
-        _tokenIds.increment();
         return newItemId;
     }
-
-    function totalSupply() public view returns (uint256) {
-        return _tokenIds.current();
-    }
-
 }
